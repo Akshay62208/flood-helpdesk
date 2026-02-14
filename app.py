4fe5323cbd90dc20fb3f0afdfaa7508e9122d294
Akshay Kumar G <akshaykumargakshay2003@gmail.com>
2026-02-14 21:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,7 @@ def submit():
 
     return 'Request Received! Thank you.'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
